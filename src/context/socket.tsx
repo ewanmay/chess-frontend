42c037f788d7698c70c1286f4e0adacf090eb4e7
Ewan May <ewan.may@ucalgary.ca>
2021-01-31 19:29:57 -0700

    change url that it hits

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -1,4 +1,4 @@
 import Socket from 'socket.io-client'
-const url = process.env.REACT_APP_BACKEND_URL || "http://localhost5000";
+const url = process.env.REACT_APP_BACKEND_URL+ ":80" || "http://localhost5000";
 export const socket: SocketIOClient.Socket = Socket(url);
 

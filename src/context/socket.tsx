f0f8614e911f856f14aaae19d9d038c1a0d1f0bc
Ewan May <ewan.may@ucalgary.ca>
2021-02-02 23:52:46 -0700

    add transport methods

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -1,4 +1,4 @@
 import Socket from 'socket.io-client'
 const url = process.env.NODE_ENV === "production" ? process.env.REACT_APP_BACKEND_URL || "" : "http://localhost:5000";
-export const socket: SocketIOClient.Socket = Socket(url);
+export const socket: SocketIOClient.Socket = Socket(url, { transports: ["websocket", "polling", "flashsocket"]});
 
